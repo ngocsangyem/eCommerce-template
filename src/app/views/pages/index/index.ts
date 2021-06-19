ae22bcafd00dc6818033f4166964f0707c1c8f65
sanggnguyenn <sang.nguyenngoc@gforces.auto>
2021-06-19 23:11:19 +0700

    add custom select

--- src/app/views/pages/index/index.ts
+++ src/app/views/pages/index/index.ts
@@ -2,42 +2,16 @@ import { FormItemSelect } from '@/components/form-item/form-item-select';
 
 export class IndexComponent {
 
-	customSelect!: FormItemSelect;
+	formCustomSelect!: FormItemSelect;
+	categoryCustomSelect!: FormItemSelect;
 
 	constructor() {
 		this.handleCustomSelect();
 	}
 
 	handleCustomSelect() {
-		const customSelect = Array.from(document.querySelectorAll('.js-select .form-control-group'));
-		if (customSelect.length > 0) {
-			const selectArray: FormItemSelect[] = [];
-			for (let i = 0; i < customSelect.length; i++) {
-				this.customSelect = new FormItemSelect(<HTMLElement>customSelect[i]);
-				selectArray.push(this.customSelect);
-			}
-
-			// listen for key events
-			window.addEventListener('keyup', (event) => {
-				if (
-					(event.keyCode && event.keyCode == 27) ||
-					(event.key && event.key.toLowerCase() == 'escape')
-				) {
-					console.log('oke');
-					// close custom select on 'Esc'
-					selectArray.forEach((element) => {
-						element.moveFocusToSelectTrigger(); // if focus is within dropdown, move it to dropdown trigger
-						element.toggleCustomSelect('false'); // close dropdown
-					});
-				}
-			});
-			// close custom select when clicking outside it
-			window.addEventListener('click', (event) => {
-				selectArray.forEach((element) => {
-					element.checkCustomSelectClick((event.target as HTMLElement));
-				});
-			});
-		}
+		this.formCustomSelect = new FormItemSelect('.form-item__select .js-select', {wrapper: 'custom-select-wrapper'});
+		this.categoryCustomSelect = new FormItemSelect('.form-item__category .js-select', {wrapper: 'custom-select-wrapper'});
 	}
 
 	static init() {
